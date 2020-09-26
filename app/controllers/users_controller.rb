0e88b3d6de1e72d4a6a638254c4f76187b50fbb8
shogo2421 <2421falsecarp@gmail.com>
2020-09-26 23:19:20 +0900

    微修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,12 +2,12 @@ class UsersController < ApplicationController
   def search
     user_search = UserSearch.new(params_user_search)
     @users = user_search.execute
-end
+  end
 
 private
 
-def params_user_search
-  params.permit(:search_name, :search_age)
-end
+  def params_user_search
+   params.permit(:search_name, :search_age)
+  end
 
 end
